bfda3bbda6a7713502136585199e9eaa7adbb445
ted555 <thanong984@gmail.com>
2018-08-14 14:13:35 +0700

    Add portfolio component file

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AppBar from '../AppBar/AppBar';
 import HomeHeader from './HomeHeader';
 import HomeArticle from './HomeArticle';
+import Portfolio from './Portfolio';
 
 class HomePage extends Component {
     render() {
@@ -10,6 +11,7 @@ class HomePage extends Component {
                 <AppBar/>
                 <HomeHeader/>
                 <HomeArticle/>
+                <Portfolio/>
             </div>
         )
     }
---
+++ src/components/Homepage/Portfolio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Portfolio.css'
+
+class Portfolio extends Component {
+    render() {
+        return (
+            <div className="Portfolio">This is a portfolio section</div>
+        );
+    }
+}
+
+export default Portfolio;

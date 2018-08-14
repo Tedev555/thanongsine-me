fe75f289d53f30481ce4c4498b4fc80d21741ad5
ted555 <thanong984@gmail.com>
2018-08-14 14:37:43 +0700

    Modified HomePage header section

--- src/components/Homepage/HomeHeader.js
+++ src/components/Homepage/HomeHeader.js
@@ -4,13 +4,13 @@ import './HomeHeader.css';
 class HomeHeader extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome To My Personal Website</h1>
+      <div className="HomeHeader">
+        <header className="HomeHeader-space">
+          <h1 className="HomeHeader-title">Welcome To My Personal Website</h1>
+          <p className="HomeHeader-intro">
+            I'm a front-end web developer from Kuala Lumpur. I am trying to make the web great again with Firebase, Progressive Web Apps, Polymer and Web Components.Also, I'm the co-organizer at Google Developer Group Kuala Lumpur. We organize conferences, meetups, workshops, and bringing all the latest Google technologies into the town.
+          </p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
